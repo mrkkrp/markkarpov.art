24b49f453442a8dc3cc4be71cdae296c8a46ba49
Mark Karpov <markkarpov92@gmail.com>
2023-12-16 21:39:42 +0100

    Style the website

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,12 +1,3 @@
-document.addEventListener("DOMContentLoaded", function(event) {
-    anchors.options = {
-        placement: 'right',
-        visible: 'always',
-        class: 'anchor'
-    };
-    anchors.add('h2, h3, h4');
-});
-
 $(document).ready(function () {
     $(".protected-email").each(function (i,v) {
         var item = $(this);
